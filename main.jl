4b7c0e650344015c511f92e7ab16d9d5a579b972
kozvojtex <51007343+kozvojtex@users.noreply.github.com>
2020-07-17 21:40:34 +0200

    Added distance from root server

--- main.jl
+++ main.jl
@@ -10,18 +10,19 @@ include("location2dict.jl")
 ##############################################################
 
 # Node of information about each domain.
-mutable struct thread_node
+struct thread_node
 	ip::String
 	dns::String
 	ports::Dict
     neighbours::Array
     location::Dict
+    depth::Int
 end
 
 # Dictionary of neighbours of root server.
 main_dict = Dict()
-# Stack of unexplored servers.
-main_stack = Stack{Array}()
+# Queue of unexplored servers.
+main_queue = Queue{Array}()
 # Array of visited vertexes.
 main_visited_vertexes = []
 
@@ -32,24 +33,28 @@ function database(root_server::Domain)
 
     # probe root
     root_dict = threatcrowd_dict(root_server)
-    tmp_neighbours = extract_neighbours(root_dict)
+    tmp_neighbours = extract_neighbours(root_dict, 0)
     root_ip = convert_dns2ip(root_server.s)
-    tmp_node = thread_node(root_ip, root_server.s, nmap(root_server.s), tmp_neighbours, location(Ip(root_ip)))
+    tmp_node = thread_node(root_ip, root_server.s, nmap(root_server.s), tmp_neighbours, location(Ip(root_ip)), 0)
     main_dict[root_server.s] = tmp_node
     append!(main_visited_vertexes, [root_server])
     # probe neighbours of root server
-    while !isempty(main_stack)
-        @show main_stack
-        vertex = pop!(main_stack)[1]
+    while !isempty(main_queue)
+        @show length(main_queue)
+        vertex_arr = dequeue!(main_queue)
+        vertex = vertex_arr[1]
+        depth = vertex_arr[2]
         vertex_dict = threatcrowd_dict(vertex)
-        tmp_neighbours = extract_neighbours(vertex_dict)
+        tmp_neighbours = extract_neighbours(vertex_dict, depth)
         if typeof(vertex) == Domain
             vertex_ip = convert_dns2ip(vertex.s)
-            tmp_node = thread_node(vertex_ip, vertex.s, nmap(vertex.s), tmp_neighbours, location(Ip(vertex_ip)))
+            tmp_node = thread_node(vertex_ip, vertex.s, nmap(vertex.s), tmp_neighbours, location(Ip(vertex_ip)), depth)
         elseif typeof(vertex) == Ip
-            tmp_node = thread_node(vertex.s, "", nmap(vertex.s), tmp_neighbours, location(Ip(vertex.s)))
+            tmp_node = thread_node(vertex.s, "", nmap(vertex.s), tmp_neighbours, location(Ip(vertex.s)), depth)
         elseif typeof(vertex) == Mail
-            tmp_node = thread_node("", vertex.s, Dict(), tmp_neighbours, Dict())
+            tmp_node = thread_node("", vertex.s, Dict(), tmp_neighbours, Dict(), depth)
+        elseif typeof(vertex) == Hash
+            tmp_node = thread_node(vertex.s, "", Dict(), tmp_neighbours, Dict(), depth)
         end
         main_dict[vertex.s] = tmp_node
     end
@@ -60,8 +65,8 @@ function database(root_server::Domain)
 	end
 end
 
-function extract_neighbours(tcMessage::Dict)
-    global main_stack
+function extract_neighbours(tcMessage::Dict, depth::Int)
+    global main_queue
     global main_visited_vertexes
     neighbours_arr = []
     if haskey(tcMessage, "resolutions")
@@ -72,7 +77,7 @@ function extract_neighbours(tcMessage::Dict)
                 if ip_address.s != "-"
                     append!(neighbours_arr, [ip_address])
                     if !(ip_address in main_visited_vertexes)
-                        push!(main_stack, [ip_address])
+                        enqueue!(main_queue, [ip_address, depth+1])
                         append!(main_visited_vertexes, [ip_address])
                     end
                 end
@@ -81,7 +86,7 @@ function extract_neighbours(tcMessage::Dict)
                 domain = Domain(neighbour["domain"])
                 append!(neighbours_arr, [domain])
                 if !(domain in main_visited_vertexes)
-                    push!(main_stack, [domain])
+                    enqueue!(main_queue, [domain, depth+1])
                     append!(main_visited_vertexes, [domain])
                 end
             end
@@ -94,7 +99,7 @@ function extract_neighbours(tcMessage::Dict)
             if length(email.s) > 0
                 append!(neighbours_arr, [email])
                 if !(email in main_visited_vertexes)
-                    push!(main_stack, [email])
+                    enqueue!(main_queue, [email, depth+1])
                     append!(main_visited_vertexes, [email])
                 end
             end
@@ -107,8 +112,21 @@ function extract_neighbours(tcMessage::Dict)
                 subdomain = Domain(subdomain)
                 append!(neighbours_arr, [subdomain])
                 if !(subdomain in main_visited_vertexes)
-                    push!(main_stack, [subdomain])
+                    enqueue!(main_queue, [subdomain, depth+1])
                     append!(main_visited_vertexes, [subdomain])
+                end
+            end
+        end
+    end
+    if haskey(tcMessage, "hashes")
+        hashes = tcMessage["hashes"]
+        for hash in hashes
+            if length(hash) > 0
+                hash = Hash(hash)
+                append!(neighbours_arr, [hash])
+                if !(hash in main_visited_vertexes)
+                    enqueue!(main_queue, [hash, depth+1])
+                    append!(main_visited_vertexes, [hash])
                 end
             end
         end
@@ -119,7 +137,7 @@ function extract_neighbours(tcMessage::Dict)
             domain = Domain(domain)
             append!(neighbours_arr, [domain])
             if !(domain in main_visited_vertexes)
-                push!(main_stack, [domain])
+                enqueue!(main_queue, [domain, depth+1])
                 append!(main_visited_vertexes, [domain])
             end
         end
